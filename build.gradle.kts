df72bca0222e4cafbb57c3f3cb3421a496d01426
Ganesh Datta <me@ganeshdatta.com>
2020-01-26 14:51:35 -0800

    Fix publication of sources

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,12 +56,13 @@ tasks {
 }
 
 val sourcesJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("sources")
     from(sourceSets.main.get().allSource)
 }
 
 publishing {
     publications {
-        create<MavenPublication>("default") {
+        register("mavenJava", MavenPublication::class) {
             from(components["java"])
             artifact(sourcesJar.get())
         }
